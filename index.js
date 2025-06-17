59478e771b2279354b8c0081c0dc5388c77de8c9
MatuezC <matuezempty@gmail.com>
2025-06-17 16:01:47 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,17 +5,22 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.post("/tebex", (req, res) => {
-  const data = req.body;
-  console.log("Webhook recibido:", data);
-
-  // Ejemplo: mostrar el nombre del jugador y el paquete
-  if (data.player && data.package) {
-    console.log(`Jugador: ${data.player.name}`);
-    console.log(`Paquete: ${data.package.name}`);
-  }
-
-  res.sendStatus(200); // Tebex espera esto
+  console.log("Webhook recibido: ", req.body);
+  res.status(200).send("OK");
 });
+
+// app.post("/tebex", (req, res) => {
+//   const data = req.body;
+//   console.log("Webhook recibido:", data);
+
+//   // Ejemplo: mostrar el nombre del jugador y el paquete
+//   if (data.player && data.package) {
+//     console.log(`Jugador: ${data.player.name}`);
+//     console.log(`Paquete: ${data.package.name}`);
+//   }
+
+//   res.sendStatus(200); // Tebex espera esto
+// });
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
